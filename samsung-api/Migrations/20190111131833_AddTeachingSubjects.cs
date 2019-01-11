742f72d6f59ee68fb99df537df6e2e7ed4b28700
Yubin Chen <roland@jongensvantechniek.nl>
2019-01-11 15:47:52 +0100

    Add migrations for Teachingsubjects

---
+++ samsung-api/Migrations/20190111131833_AddTeachingSubjects.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace samsung.api.Migrations
+{
+    public partial class AddTeachingSubjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeachingSubjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeachingSubjects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeachingSubjects");
+        }
+    }
+}

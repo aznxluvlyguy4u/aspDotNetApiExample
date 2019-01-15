e8a37e6944e47688db0cd5fc0f7a7cefd970619c
Yubin Chen <yubin@jongensvantechniek.nl>
2019-01-15 16:30:26 +0100

    Add generalUserTeachingSubject Migration

--- Samsung-Api-Aws/Migrations/20190115152441_addGeneralUserTeachingSubject.Designer.cs
+++ Samsung-Api-Aws/Migrations/20190115152441_addGeneralUserTeachingSubject.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using samsung.api.DataSource;
 
 namespace samsung.api.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190115152441_addGeneralUserTeachingSubject")]
+    partial class addGeneralUserTeachingSubject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

5d58afd0fc27fe21d22e7e5cb6f497aabece62c1
Yubin Chen <yubin@jongensvantechniek.nl>
2019-01-18 14:16:51 +0100

    Add Interests and related migrations

---
+++ Samsung-Api-Aws/Migrations/20190118131517_AddInterest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace samsung.api.Migrations
+{
+    public partial class AddInterest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GeneralUserInterest",
+                columns: table => new
+                {
+                    GeneralUserId = table.Column<int>(nullable: false),
+                    InterestId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralUserInterest", x => new { x.GeneralUserId, x.InterestId });
+                    table.ForeignKey(
+                        name: "FK_GeneralUserInterest_GeneralUsers_GeneralUserId",
+                        column: x => x.GeneralUserId,
+                        principalTable: "GeneralUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GeneralUserInterest_Interests_InterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Interests",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Interest 1" });
+
+            migrationBuilder.InsertData(
+                table: "Interests",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Interest 2" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneralUserInterest_InterestId",
+                table: "GeneralUserInterest",
+                column: "InterestId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeneralUserInterest");
+
+            migrationBuilder.DropTable(
+                name: "Interests");
+        }
+    }
+}

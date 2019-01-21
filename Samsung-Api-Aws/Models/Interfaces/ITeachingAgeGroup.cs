96b65c3e9b33105247af4ffdb1f813a9e0954925
Yubin Chen <yubin@jongensvantechniek.nl>
2019-01-21 19:59:33 +0100

    Add GET api/v1/TeachingAgeGroups

--- Samsung-Api-Aws/Models/Interfaces/ITeachingAgeGroup.cs
+++ Samsung-Api-Aws/Models/Interfaces/ITeachingAgeGroup.cs
@@ -1,6 +1,6 @@
 ﻿namespace samsung_api.Models.Interfaces
 {
-    public interface IAgeGroup
+    public interface ITeachingAgeGroup
     {
         int Id { get; set; }
         string Name { get; set; }
---
+++ Samsung-Api-Aws/Models/Response/GetTeachingAgeGroupsResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace samsung.api.Models.Response
+{
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class GetTeachingAgeGroupsResponse
+    {
+        public GetTeachingAgeGroupsResponse()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}

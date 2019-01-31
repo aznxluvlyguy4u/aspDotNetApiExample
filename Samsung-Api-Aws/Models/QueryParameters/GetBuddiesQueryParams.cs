b975e6a4654f60aa48f6675c09b78141eac192c0
Yubin Chen <yubin@jongensvantechniek.nl>
2019-01-31 15:05:29 +0100

    Rename buddy request

---
+++ Samsung-Api-Aws/Models/QueryParameters/GetBuddiesQueryParams.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamsungApiAws.Models.QueryParameters
+{
+    public class GetBuddiesQueryParams
+    {
+        [BindRequired]
+        public int state { get; set; }
+    }
+}
